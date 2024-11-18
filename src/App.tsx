c4d9b8512d95257ed0132c5fcbcd6c395863df3c
Maanlicht91 <aynurdemir1991@hotmail.com>
2024-11-18 20:43:49 +0300

    fix/UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import Movie from "./Layout/Movie";
 import BtnGroupBottom from "./Layout/BtnGroupBottom";
 import Modal from "./Layout/Modals";
 import { useAppDispatch, useAppSelector } from "./store";
-import {isVideoEnded, VideoPlay} from "./slices/VideoPlay";
+import { isVideoEnded, VideoPlay } from "./slices/VideoPlay";
 import result from "./imgdata";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   const [view, SetView] = useState(false);
   const play = useAppSelector((state) => state.video.view);
   const ended = useAppSelector((state) => state.video.video_ended);
@@ -23,7 +23,7 @@ const App:React.FC = () => {
   const VideoStart = () => {
     dispatch(VideoPlay());
     dispatch(isVideoEnded());
-  }
+  };
 
   useEffect(() => {
     const handleClick = (event: MouseEvent) => {
@@ -33,51 +33,49 @@ const App:React.FC = () => {
       }
     };
 
-    document.addEventListener('click', handleClick);
+    document.addEventListener("click", handleClick);
 
     return () => {
-      document.removeEventListener('click', handleClick);
+      document.removeEventListener("click", handleClick);
     };
-  }, [])
+  }, []);
 
-  return(
+  return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
       <BtnGroupTop isplay={play} />
       <Movie isplay={play} />
       {
-        
-          <img 
-            className={`cursor-pointer fixed top-[45px] left-[45px] w-[88px] h-[88px] z-40 duration-[400] transition-all hover:opacity-i_0 ${play && "opacity-50"}`} 
-            src="/assets/image.png"
-            alt="logo"  
-          />
+        <img
+          className={`cursor-pointer fixed top-[45px] left-[45px] w-[88px] h-[88px] z-40 duration-[400] transition-all hover:opacity-i_0 ${
+            play && "opacity-50"
+          }`}
+          src="/assets/image.png"
+          alt="logo"
+        />
       }
-      {
-        !ended &&
-          <img 
-            className="hover:opacity-i_0 fixed top-[45px] w-[100px] right-[45px] z-40 duration-[400] transition-all cursor-pointer" 
-            src={play ? result.stopbtn : result.playbtn}
-            alt="btn"
-            onClick={() => dispatch(VideoPlay())}
-          />
-      }
+      {!ended && (
+        <img
+          className="hover:opacity-i_0 fixed top-[45px] w-[100px] right-[45px] z-40 duration-[400] transition-all cursor-pointer"
+          src={play ? result.stopbtn : result.playbtn}
+          alt="btn"
+          onClick={() => dispatch(VideoPlay())}
+        />
+      )}
       <Loader view={view} />
 
-
-      {
-        !play && ended &&
-          <img 
-            className="cursor-pointer absolute transition-all hover:brightness-125" 
-            src="/assets/play-DqB0jiWs.png" 
-            alt=""
-            onClick={VideoStart}
-          />
-      }
+      {!play && ended && (
+        <img
+          className="cursor-pointer absolute transition-all hover:brightness-125"
+          src="/assets/play-DqB0jiWs.png"
+          alt=""
+          onClick={VideoStart}
+        />
+      )}
       <BtnGroupBottom isplay={play} />
+
       <Modal />
     </div>
   );
 };
 
 export default App;
-  
